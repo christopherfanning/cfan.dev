a69fdd3f697ec366d3cc8f395e5c66c4ea4d1905
Christopher Fanning <chris@christopherfanning@gmail.com>
2022-04-03 12:15:33 -0500

    rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cfan.dev'),
+      dir: require('path').join(__dirname, './coverage/cfan'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cfan.dev'`, () => {
+  it(`should have as title 'cfan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('cfan.dev');
